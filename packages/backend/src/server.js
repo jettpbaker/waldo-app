195bcc1c19e15a3ff68e0284d284919fefeecd05
jettpbaker <jett.p.baker@gmail.com>
2025-05-01 17:57:42 +1000

    setup cors on the server

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get("/api/ping", (req, res) => {
   res.json({ message: "pong" });
