66fcfd982c5bde8eb07510c5c347870ece60b165
jettpbaker <jett.p.baker@gmail.com>
2025-05-02 15:45:09 +1000

    configuration

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,19 +1,19 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+console.log(process.env.FRONTEND_URL)
+
+const app = express()
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
-};
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.get("/api/ping", (req, res) => {
-  res.json({ message: "pong" });
-});
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'pong' })
+})
 
-app.listen(3000, () =>
-  console.log(`Backend listening on http://localhost:3000`)
-);
+app.listen(3000, () => console.log(`Backend listening on port 3000`))
